7efed5b98c19bbad5ebcf82addd65c9807766bd6
Syed Irfan kazmi <irfankazmielectron@gmail.com>
2024-09-26 14:58:11 +0500

    weew
    ewrefff

--- layers/layers.js
+++ layers/layers.js
@@ -30,9 +30,9 @@ var lyr_TerrorAttacksterror_attacks_1 = new ol.layer.Vector({
 
 lyr_GoogleStandardMap_0.setVisible(true);lyr_TerrorAttacksterror_attacks_1.setVisible(true);
 var layersList = [lyr_GoogleStandardMap_0,lyr_TerrorAttacksterror_attacks_1];
-lyr_TerrorAttacksterror_attacks_1.set('fieldAliases', {'fid': 'fid', 'area': 'area', 'date & time': 'date & time', 'perperator group': 'perperator group', 'target': 'target', 'fatalities': 'fatalities', 'injured': 'injured', 'attack type': 'attack type', 'weapon type': 'weapon type', });
-lyr_TerrorAttacksterror_attacks_1.set('fieldImages', {'fid': 'TextEdit', 'area': 'TextEdit', 'date & time': 'DateTime', 'perperator group': 'TextEdit', 'target': 'TextEdit', 'fatalities': 'Range', 'injured': 'Range', 'attack type': 'TextEdit', 'weapon type': 'TextEdit', });
-lyr_TerrorAttacksterror_attacks_1.set('fieldLabels', {'fid': 'no label', 'area': 'no label', 'date & time': 'no label', 'perperator group': 'no label', 'target': 'no label', 'fatalities': 'no label', 'injured': 'no label', 'attack type': 'no label', 'weapon type': 'no label', });
+lyr_TerrorAttacksterror_attacks_1.set('fieldAliases', {'fid': 'fid', 'area': 'area', 'attack type': 'attack type', });
+lyr_TerrorAttacksterror_attacks_1.set('fieldImages', {'fid': 'TextEdit', 'area': 'TextEdit', 'attack type': 'TextEdit', });
+lyr_TerrorAttacksterror_attacks_1.set('fieldLabels', {'fid': 'no label', 'area': 'no label', 'attack type': 'no label', });
 lyr_TerrorAttacksterror_attacks_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
