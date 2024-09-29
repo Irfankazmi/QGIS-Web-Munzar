be3430dd941b20e50928d69fae82c8c205eb7071
Syed Irfan kazmi <60047611+Irfankazmi@users.noreply.github.com>
2024-09-30 03:19:21 +0500

    3
    3

--- layers/layers.js
+++ layers/layers.js
@@ -12,27 +12,27 @@ var wms_layers = [];
                 url: 'https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}'
             })
         });
-var format_TerrorAttacksterror_attacks__terror_attacks_1 = new ol.format.GeoJSON();
-var features_TerrorAttacksterror_attacks__terror_attacks_1 = format_TerrorAttacksterror_attacks__terror_attacks_1.readFeatures(json_TerrorAttacksterror_attacks__terror_attacks_1, 
+var format_TerrorAttacksterror_attacks_1 = new ol.format.GeoJSON();
+var features_TerrorAttacksterror_attacks_1 = format_TerrorAttacksterror_attacks_1.readFeatures(json_TerrorAttacksterror_attacks_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_TerrorAttacksterror_attacks__terror_attacks_1 = new ol.source.Vector({
+var jsonSource_TerrorAttacksterror_attacks_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_TerrorAttacksterror_attacks__terror_attacks_1.addFeatures(features_TerrorAttacksterror_attacks__terror_attacks_1);
-var lyr_TerrorAttacksterror_attacks__terror_attacks_1 = new ol.layer.Vector({
+jsonSource_TerrorAttacksterror_attacks_1.addFeatures(features_TerrorAttacksterror_attacks_1);
+var lyr_TerrorAttacksterror_attacks_1 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_TerrorAttacksterror_attacks__terror_attacks_1, 
-                style: style_TerrorAttacksterror_attacks__terror_attacks_1,
-                popuplayertitle: "Terror Attacks — terror_attacks__terror_attacks",
+                source:jsonSource_TerrorAttacksterror_attacks_1, 
+                style: style_TerrorAttacksterror_attacks_1,
+                popuplayertitle: "Terror Attacks — terror_attacks",
                 interactive: true,
-                title: '<img src="styles/legend/TerrorAttacksterror_attacks__terror_attacks_1.png" /> Terror Attacks — terror_attacks__terror_attacks'
+                title: '<img src="styles/legend/TerrorAttacksterror_attacks_1.png" /> Terror Attacks — terror_attacks'
             });
 
-lyr_GoogleStandardMap_0.setVisible(true);lyr_TerrorAttacksterror_attacks__terror_attacks_1.setVisible(true);
-var layersList = [lyr_GoogleStandardMap_0,lyr_TerrorAttacksterror_attacks__terror_attacks_1];
-lyr_TerrorAttacksterror_attacks__terror_attacks_1.set('fieldAliases', {'fid': 'fid', 'qc_id': 'qc_id', 'area': 'area', });
-lyr_TerrorAttacksterror_attacks__terror_attacks_1.set('fieldImages', {'fid': 'TextEdit', 'qc_id': 'Range', 'area': 'TextEdit', });
-lyr_TerrorAttacksterror_attacks__terror_attacks_1.set('fieldLabels', {'fid': 'no label', 'qc_id': 'no label', 'area': 'no label', });
-lyr_TerrorAttacksterror_attacks__terror_attacks_1.on('precompose', function(evt) {
+lyr_GoogleStandardMap_0.setVisible(true);lyr_TerrorAttacksterror_attacks_1.setVisible(true);
+var layersList = [lyr_GoogleStandardMap_0,lyr_TerrorAttacksterror_attacks_1];
+lyr_TerrorAttacksterror_attacks_1.set('fieldAliases', {'fid': 'fid', 'area': 'area', 'date & time': 'date & time', 'perperator group': 'perperator group', 'target': 'target', 'fatalities': 'fatalities', 'injured': 'injured', 'attack type': 'attack type', 'weapon type': 'weapon type', });
+lyr_TerrorAttacksterror_attacks_1.set('fieldImages', {'fid': 'TextEdit', 'area': 'TextEdit', 'date & time': 'DateTime', 'perperator group': 'TextEdit', 'target': 'TextEdit', 'fatalities': 'Range', 'injured': 'Range', 'attack type': 'TextEdit', 'weapon type': 'TextEdit', });
+lyr_TerrorAttacksterror_attacks_1.set('fieldLabels', {'fid': 'no label', 'area': 'no label', 'date & time': 'no label', 'perperator group': 'no label', 'target': 'no label', 'fatalities': 'no label', 'injured': 'no label', 'attack type': 'no label', 'weapon type': 'no label', });
+lyr_TerrorAttacksterror_attacks_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
