03c72333c9f2a93abed12d81c522efbfa3f81adc
Syed Irfan kazmi <irfankazmielectron@gmail.com>
2024-10-03 11:24:05 +0500

    Presentation 1

--- layers/layers.js
+++ layers/layers.js
@@ -12,33 +12,45 @@ var wms_layers = [];
                 url: 'https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}'
             })
         });
-var format_TerrorAttacks_1 = new ol.format.GeoJSON();
-var features_TerrorAttacks_1 = format_TerrorAttacks_1.readFeatures(json_TerrorAttacks_1, 
+var format_PoliticalEvents_1 = new ol.format.GeoJSON();
+var features_PoliticalEvents_1 = format_PoliticalEvents_1.readFeatures(json_PoliticalEvents_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_TerrorAttacks_1 = new ol.source.Vector({
+var jsonSource_PoliticalEvents_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_TerrorAttacks_1.addFeatures(features_TerrorAttacks_1);
-var lyr_TerrorAttacks_1 = new ol.layer.Vector({
+jsonSource_PoliticalEvents_1.addFeatures(features_PoliticalEvents_1);
+var lyr_PoliticalEvents_1 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_TerrorAttacks_1, 
-                style: style_TerrorAttacks_1,
+                source:jsonSource_PoliticalEvents_1, 
+                style: style_PoliticalEvents_1,
+                popuplayertitle: "Political Events",
+                interactive: true,
+                title: '<img src="styles/legend/PoliticalEvents_1.png" /> Political Events'
+            });
+var format_TerrorAttacks_2 = new ol.format.GeoJSON();
+var features_TerrorAttacks_2 = format_TerrorAttacks_2.readFeatures(json_TerrorAttacks_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_TerrorAttacks_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_TerrorAttacks_2.addFeatures(features_TerrorAttacks_2);
+var lyr_TerrorAttacks_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_TerrorAttacks_2, 
+                style: style_TerrorAttacks_2,
                 popuplayertitle: "Terror Attacks",
                 interactive: true,
-    title: 'Terror Attacks<br />\
-    <img src="styles/legend/TerrorAttacks_1_0.png" /> Multan<br />\
-    <img src="styles/legend/TerrorAttacks_1_1.png" /> dsfsdf<br />\
-    <img src="styles/legend/TerrorAttacks_1_2.png" /> fgdg<br />\
-    <img src="styles/legend/TerrorAttacks_1_3.png" /> Hyderabad<br />\
-    <img src="styles/legend/TerrorAttacks_1_4.png" /> Lahore<br />\
-    <img src="styles/legend/TerrorAttacks_1_5.png" /> <br />'
-        });
+                title: '<img src="styles/legend/TerrorAttacks_2.png" /> Terror Attacks'
+            });
 
-lyr_GoogleStandardMap_0.setVisible(true);lyr_TerrorAttacks_1.setVisible(true);
-var layersList = [lyr_GoogleStandardMap_0,lyr_TerrorAttacks_1];
-lyr_TerrorAttacks_1.set('fieldAliases', {'fid': 'fid', 'area': 'area', 'Detail': 'Detail', });
-lyr_TerrorAttacks_1.set('fieldImages', {'fid': 'TextEdit', 'area': 'TextEdit', 'Detail': 'TextEdit', });
-lyr_TerrorAttacks_1.set('fieldLabels', {'fid': 'hidden field', 'area': 'inline label - always visible', 'Detail': 'inline label - always visible', });
-lyr_TerrorAttacks_1.on('precompose', function(evt) {
+lyr_GoogleStandardMap_0.setVisible(true);lyr_PoliticalEvents_1.setVisible(true);lyr_TerrorAttacks_2.setVisible(true);
+var layersList = [lyr_GoogleStandardMap_0,lyr_PoliticalEvents_1,lyr_TerrorAttacks_2];
+lyr_PoliticalEvents_1.set('fieldAliases', {'fid': 'fid', 'Location': 'Location', 'Protest By': 'Protest By', 'No. of  Protestors': 'No. of  Protestors', 'Detail': 'Detail', 'Media': 'Media', });
+lyr_TerrorAttacks_2.set('fieldAliases', {'fid': 'fid', 'Location': 'Location', 'Type of Attack': 'Type of Attack', 'No. of Injuries': 'No. of Injuries', 'No. of Deaths': 'No. of Deaths', 'Detail': 'Detail', 'Media': 'Media', });
+lyr_PoliticalEvents_1.set('fieldImages', {'fid': 'TextEdit', 'Location': 'TextEdit', 'Protest By': 'TextEdit', 'No. of  Protestors': 'TextEdit', 'Detail': 'TextEdit', 'Media': 'TextEdit', });
+lyr_TerrorAttacks_2.set('fieldImages', {'fid': 'TextEdit', 'Location': '', 'Type of Attack': '', 'No. of Injuries': '', 'No. of Deaths': '', 'Detail': '', 'Media': '', });
+lyr_PoliticalEvents_1.set('fieldLabels', {'fid': 'no label', 'Location': 'inline label - always visible', 'Protest By': 'inline label - always visible', 'No. of  Protestors': 'inline label - always visible', 'Detail': 'inline label - always visible', 'Media': 'inline label - always visible', });
+lyr_TerrorAttacks_2.set('fieldLabels', {'fid': 'no label', 'Location': 'inline label - always visible', 'Type of Attack': 'inline label - always visible', 'No. of Injuries': 'inline label - always visible', 'No. of Deaths': 'inline label - always visible', 'Detail': 'inline label - always visible', 'Media': 'inline label - always visible', });
+lyr_TerrorAttacks_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- styles/TerrorAttacks_2_style.js
+++ styles/TerrorAttacks_2_style.js
@@ -21,8 +21,14 @@ var style_TerrorAttacks_2 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 8.0 + size,
-            stroke: new ol.style.Stroke({color: 'rgba(128,17,25,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}), fill: new ol.style.Fill({color: 'rgba(219,30,42,1.0)'})}),
+        image: new ol.style.RegularShape({radius: 13.2 + size, points: 5,
+            radius2: 6.6, stroke: new ol.style.Stroke({color: 'rgba(184,8,8,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(184,8,8,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    }),new ol.style.Style({
+        image: new ol.style.RegularShape({radius: 14.4 + size, points: 5,
+            radius2: 7.2, stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
