cabf96e91e68cae7f0ad0bf22f7911bc86312a34
Syed Irfan kazmi <60047611+Irfankazmi@users.noreply.github.com>
2024-10-10 16:25:54 +0500

    dsfsd

--- layers/layers.js
+++ layers/layers.js
@@ -27,48 +27,48 @@ var lyr_District_Boundary_1 = new ol.layer.Vector({
                 interactive: true,
                 title: '<img src="styles/legend/District_Boundary_1.png" /> District_Boundary'
             });
-var format_TerrorAttacks_1_2 = new ol.format.GeoJSON();
-var features_TerrorAttacks_1_2 = format_TerrorAttacks_1_2.readFeatures(json_TerrorAttacks_1_2, 
+var format_terror_attacks_2 = new ol.format.GeoJSON();
+var features_terror_attacks_2 = format_terror_attacks_2.readFeatures(json_terror_attacks_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_TerrorAttacks_1_2 = new ol.source.Vector({
+var jsonSource_terror_attacks_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_TerrorAttacks_1_2.addFeatures(features_TerrorAttacks_1_2);
-var lyr_TerrorAttacks_1_2 = new ol.layer.Vector({
+jsonSource_terror_attacks_2.addFeatures(features_terror_attacks_2);
+var lyr_terror_attacks_2 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_TerrorAttacks_1_2, 
-                style: style_TerrorAttacks_1_2,
-                popuplayertitle: "Terror Attacks_1",
+                source:jsonSource_terror_attacks_2, 
+                style: style_terror_attacks_2,
+                popuplayertitle: "terror_attacks",
                 interactive: true,
-                title: '<img src="styles/legend/TerrorAttacks_1_2.png" /> Terror Attacks_1'
+                title: '<img src="styles/legend/terror_attacks_2.png" /> terror_attacks'
             });
-var format_PoliticalEvents_3 = new ol.format.GeoJSON();
-var features_PoliticalEvents_3 = format_PoliticalEvents_3.readFeatures(json_PoliticalEvents_3, 
+var format_protests_3 = new ol.format.GeoJSON();
+var features_protests_3 = format_protests_3.readFeatures(json_protests_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_PoliticalEvents_3 = new ol.source.Vector({
+var jsonSource_protests_3 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_PoliticalEvents_3.addFeatures(features_PoliticalEvents_3);
-var lyr_PoliticalEvents_3 = new ol.layer.Vector({
+jsonSource_protests_3.addFeatures(features_protests_3);
+var lyr_protests_3 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource_PoliticalEvents_3, 
-                style: style_PoliticalEvents_3,
-                popuplayertitle: "Political Events",
+                source:jsonSource_protests_3, 
+                style: style_protests_3,
+                popuplayertitle: "protests",
                 interactive: true,
-                title: '<img src="styles/legend/PoliticalEvents_3.png" /> Political Events'
+                title: '<img src="styles/legend/protests_3.png" /> protests'
             });
 
-lyr_GoogleStandardMap_0.setVisible(true);lyr_District_Boundary_1.setVisible(true);lyr_TerrorAttacks_1_2.setVisible(true);lyr_PoliticalEvents_3.setVisible(true);
-var layersList = [lyr_GoogleStandardMap_0,lyr_District_Boundary_1,lyr_TerrorAttacks_1_2,lyr_PoliticalEvents_3];
+lyr_GoogleStandardMap_0.setVisible(true);lyr_District_Boundary_1.setVisible(true);lyr_terror_attacks_2.setVisible(true);lyr_protests_3.setVisible(true);
+var layersList = [lyr_GoogleStandardMap_0,lyr_District_Boundary_1,lyr_terror_attacks_2,lyr_protests_3];
 lyr_District_Boundary_1.set('fieldAliases', {'OBJECTID': 'OBJECTID', 'PROVINCE': 'PROVINCE', 'DISTRICT': 'DISTRICT', 'Shape_Leng': 'Shape_Leng', 'ADMIN_UNIT': 'ADMIN_UNIT', 'Shape_Le_1': 'Shape_Le_1', 'Shape_Le_2': 'Shape_Le_2', 'Shape_Area': 'Shape_Area', });
-lyr_TerrorAttacks_1_2.set('fieldAliases', {'fid': 'fid', 'Area': 'Area', });
-lyr_PoliticalEvents_3.set('fieldAliases', {'fid': 'fid', 'Party': 'Party', });
+lyr_terror_attacks_2.set('fieldAliases', {'fid': 'fid', 'area': 'area', 'date & time': 'date & time', 'perperator group': 'perperator group', 'target': 'target', 'fatalities': 'fatalities', 'injured': 'injured', 'attack type': 'attack type', 'weapon type': 'weapon type', });
+lyr_protests_3.set('fieldAliases', {'fid': 'fid', });
 lyr_District_Boundary_1.set('fieldImages', {'OBJECTID': 'Range', 'PROVINCE': 'TextEdit', 'DISTRICT': 'TextEdit', 'Shape_Leng': 'TextEdit', 'ADMIN_UNIT': 'TextEdit', 'Shape_Le_1': 'TextEdit', 'Shape_Le_2': 'TextEdit', 'Shape_Area': 'TextEdit', });
-lyr_TerrorAttacks_1_2.set('fieldImages', {'fid': 'TextEdit', 'Area': 'TextEdit', });
-lyr_PoliticalEvents_3.set('fieldImages', {'fid': 'TextEdit', 'Party': 'TextEdit', });
-lyr_District_Boundary_1.set('fieldLabels', {'OBJECTID': 'hidden field', 'PROVINCE': 'hidden field', 'DISTRICT': 'inline label - always visible', 'Shape_Leng': 'hidden field', 'ADMIN_UNIT': 'hidden field', 'Shape_Le_1': 'hidden field', 'Shape_Le_2': 'hidden field', 'Shape_Area': 'hidden field', });
-lyr_TerrorAttacks_1_2.set('fieldLabels', {'fid': 'hidden field', 'Area': 'inline label - always visible', });
-lyr_PoliticalEvents_3.set('fieldLabels', {'fid': 'no label', 'Party': 'no label', });
-lyr_PoliticalEvents_3.on('precompose', function(evt) {
+lyr_terror_attacks_2.set('fieldImages', {'fid': 'TextEdit', 'area': 'TextEdit', 'date & time': 'DateTime', 'perperator group': 'TextEdit', 'target': 'TextEdit', 'fatalities': 'Range', 'injured': 'Range', 'attack type': 'TextEdit', 'weapon type': 'TextEdit', });
+lyr_protests_3.set('fieldImages', {'fid': 'TextEdit', });
+lyr_District_Boundary_1.set('fieldLabels', {'OBJECTID': 'hidden field', 'PROVINCE': 'hidden field', 'DISTRICT': 'no label', 'Shape_Leng': 'hidden field', 'ADMIN_UNIT': 'hidden field', 'Shape_Le_1': 'hidden field', 'Shape_Le_2': 'hidden field', 'Shape_Area': 'hidden field', });
+lyr_terror_attacks_2.set('fieldLabels', {'fid': 'no label', 'area': 'no label', 'date & time': 'no label', 'perperator group': 'no label', 'target': 'no label', 'fatalities': 'no label', 'injured': 'no label', 'attack type': 'no label', 'weapon type': 'no label', });
+lyr_protests_3.set('fieldLabels', {'fid': 'no label', });
+lyr_protests_3.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- styles/protests_3_style.js
+++ styles/protests_3_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_District_Boundary_1 = function(feature, resolution){
+var style_protests_3 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -21,7 +21,8 @@ var style_District_Boundary_1 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(10,248,236,0.19607843137254902)'}),
+        image: new ol.style.RegularShape({radius: 14.4 + size, points: 5,
+            radius2: 7.2, stroke: new ol.style.Stroke({color: 'rgba(255,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.76}), fill: new ol.style.Fill({color: 'rgba(255,0,0,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
