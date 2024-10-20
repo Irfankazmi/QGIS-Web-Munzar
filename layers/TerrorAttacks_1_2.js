d6482edfbe6d19f231a1d0149585af2ea1cb60a1
Syed Irfan kazmi <60047611+Irfankazmi@users.noreply.github.com>
2024-10-21 01:51:13 +0500

    Spatial 1

--- layers/TerrorAttacks_1_2.js
+++
@@ -1,9 +0,0 @@
-var json_TerrorAttacks_1_2 = {
-"type": "FeatureCollection",
-"name": "TerrorAttacks_1_2",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "fid": "Autogenerate", "Area": "Peshawar" }, "geometry": { "type": "Point", "coordinates": [ 71.52490234375, 34.015098571777344 ] } },
-{ "type": "Feature", "properties": { "fid": "2", "Area": "Karachi" }, "geometry": { "type": "Point", "coordinates": [ 67.244913716639203, 25.017555024916362 ] } }
-]
-}
--- layers/TerrorAttacksterror_attacks_1.js
+++
@@ -1,1 +0,0 @@
-var json_TerrorAttacksterror_attacks_1 = {"type":"FeatureCollection","name":"TerrorAttacksterror_attacks_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"60","area":"dsfsdf"},"geometry":{"type":"MultiPoint","coordinates":[[70.400968133478301,32.836093472608766]]}}]}
